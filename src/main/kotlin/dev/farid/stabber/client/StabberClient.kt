3e0081666647a2e785a14c4a7fde08b5b1bfb3a5
Farid Nurrahman <fnur0006@student.monash.edu>
2026-08-15 17:19:33 +1000

    feat: wire pathfinding on client

--- src/main/kotlin/dev/farid/stabber/client/StabberClient.kt
+++ src/main/kotlin/dev/farid/stabber/client/StabberClient.kt
@@ -1,10 +1,21 @@
 package dev.farid.stabber.client
 
+import dev.farid.stabber.client.path.PathfindingController
+import dev.farid.stabber.client.render.PathGizmoRenderer
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents
 
 class StabberClient : ClientModInitializer {
 
     override fun onInitializeClient() {
         StabberKeys.register()
+        ClientTickEvents.END_CLIENT_TICK.register { client ->
+            PathfindingController.tick(client)
+            PathGizmoRenderer.render(PathfindingController.path)
+        }
+        ClientPlayConnectionEvents.DISCONNECT.register { _, _ ->
+            PathfindingController.onDisconnect()
+        }
     }
 }
